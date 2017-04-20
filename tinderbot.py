2711cba776fb6141078ea126f3b313147db24eaf
djnugent <djnugent@iastate.edu>
2017-04-20 16:52:03 -0500

    Initial Commit

---
+++ tinderbot.py
@@ -0,0 +1,86 @@
+import pynder
+from time import gmtime, strftime, sleep
+import subprocess
+import os
+import requests
+
+
+class Tinderbot():
+
+    SWIPE_RATE = 3
+
+    def __init__(self,access_token,user_id,target,loc,interval):
+        self.access_token = access_token
+        self.user_id = user_id
+        self.target = target
+        self.loc = loc
+        self.interval = interval
+        self.server = None
+
+    def start(self):
+        #start file server
+        self.server = subprocess.Popen(['python3 -m http.server 8000'], shell=True)
+
+        #start tinder session
+        print("Starting Session")
+        self.session = pynder.Session(fb_id, access_token)
+        print("Setting location")
+        print(self.session.update_location(self.loc[0],self.loc[1])) # updates latitude and longitude for your profile
+        print("Logged in as: " + str(self.session.profile))  # your profile. If you update its attributes they will be updated on Tinder.
+        print("Searching for: " + self.target + " every " + str(self.interval) + " minutes")
+
+
+    def swipe_session(self):
+        users = self.session.nearby_users(limit =10 ) # returns a list of users nearby
+        count = 0
+        for user in users:
+            if(user.name == target):
+                self.process_hit(user)
+                self.session.dislike(user)
+                print("swipe")
+                time.sleep(1.0/self.SWIPE_RATE)
+            count += 1
+        t = strftime("%a, %d %b %Y %H:%M:%S", gmtime())
+        print(t + " - Swiped " + str(count) + "users")
+
+    def process_hit(self,user):
+            pic_url = user.photos[0]
+            img_data = requests.get(pic_url).content
+            file_loc = "hits/" + self.pwd + "/" + str(user.id) + ".jpg"
+            with open(file_loc, 'wb') as handler:
+                handler.write(img_data)
+            print(">>>HIT: id: " + str(user.id) + " age: " + str(user.age) + " photoURl: " + pic_url)
+
+    def check_state(self):
+        current_date = "hits/" + strftime("%d-%b-%Y", gmtime())
+        if not os.path.isdir(current_date):
+            print("Making directory " + current_date)
+            os.mkdir(current_date)
+            self.pwd = current_date
+
+    def run(self):
+        try:
+            #start file server
+            self.start()
+
+            while True:
+                self.check_state()
+                self.swipe_session()
+                sleep(60 * interval)
+
+        finally:
+            self.server.kill()
+
+
+
+
+
+if __name__== "__main__":
+    access_token = "912ef5de-9791-4918-bb3d-eee397066399"
+    fb_id = 1021804951
+    loc = 44.942594, -93.191030
+    target = "Kim"
+    interval = 240
+
+    bot = Tinderbot(access_token,fb_id,loc,target,interval)
+    bot.run()
